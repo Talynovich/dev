3b71ea89f39123b489e6cd774eaf8b3d76005da8
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2025-11-13 21:03:31 +0600

    js lesson1 step1

---
+++ js/lesson1/script.js
@@ -0,0 +1,23 @@
+let name = 'Tolya'
+let age = 24
+let avgscore = 50.5
+let rainyday = true
+let nullValue = null
+let undefinedValue
+
+let initialNumber = 30
+initialNumber = (initialNumber += 10)
+initialNumber = (initialNumber *= 2)
+
+let initialString = 'Я изучаю'
+initialString = initialString  += " JavaScript!"
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof avgscore)
+console.log(typeof rainyday)
+console.log(typeof nullValue)
+console.log(typeof undefinedValue)
+console.log( initialNumber)
+console.log( initialString)
+
