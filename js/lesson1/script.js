d8a41e9e3c72130290344f09a2520facc9cf8add
a.bervenov <linelimefine@gmail.com>
2025-11-13 22:59:13 +0600

    updated js lesson1 step3

--- js/lesson1/script.js
+++ js/lesson1/script.js
@@ -12,14 +12,14 @@ initialNumber = (initialNumber *= 2)
 let initialString = 'Я изучаю'
 initialString = initialString  += " JavaScript!"
 
-console.log(typeof name)
-console.log(typeof age)
-console.log(typeof avgscore)
-console.log(typeof rainyday)
-console.log(typeof nullValue)
-console.log(typeof undefinedValue)
-console.log( initialNumber)
-console.log( initialString)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof avgscore)
+// console.log(typeof rainyday)
+// console.log(typeof nullValue)
+// console.log(typeof undefinedValue)
+// console.log( initialNumber)
+// console.log( initialString)
 
 // step 2
 
@@ -34,6 +34,38 @@ let b = 7
 let radius = 5
 let PI = 3.14159
 let area = PI * (radius ** 2);
-console.log(area)
+// console.log(area)
+
+
+//  step 3
+
+
+// Преобразуйте строку "77" в число с помощью Number(
+// let string = '10'
+// let number = Number(string)
+// console.log(typeof number)
+
+// Преобразуйте число 99 в строку с помощью String()
+// let string = 10
+// let number = String(string)
+// console.log(typeof number)
+
+//Преобразуйте число 0 и строку "0" в булево значение с помощью Boolean().
+// let number = 0
+// let string = 'O'
+// let numberoolan = Boolean(number)
+// let stringboolean = Boolean(string)
+// console.log(numberoolan, stringboolean)
+
+//Преобразуйте строку "Non-empty" в булево значение.
+let string = 'Non-empty'
+let boolean = Boolean(string)
+console.log(boolean)
+
+// let string = '15'
+// let number = 20
+// let result = (string + number)
+// console.log(result)
+
 
 
