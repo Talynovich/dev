d243b17347528b0d95a714044d12b8fee669de07
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2025-11-16 19:47:50 +0600

    finished part3

--- js/conditions/main.js
+++ js/conditions/main.js
@@ -1,16 +1,16 @@
 let numberToCheck = 18
 
 if (numberToCheck % 2 === 0) {
-    console.log(`Число ${numberToCheck} — четное.`);
+    console.log(`Число ${numberToCheck} — четное.`)
 } else {
-    console.log(`Число ${numberToCheck} — нечетное.`);
+    console.log(`Число ${numberToCheck} — нечетное.`)
 }
 
-let value = -5 
+let value = -5
 
-if(value < 0){
+if (value < 0) {
     console.log(`Число <<${value}>> отрицательное`)
-} else if (value > 0){
+} else if (value > 0) {
     console.log(`Число <<${value}>> положительное`)
 } else {
     console.log('Число равно нулю')
@@ -19,11 +19,13 @@ if(value < 0){
 let stringNumber = "45"
 let threshold = 50
 
-if (Number(stringNumber) > threshold ){
+if (Number(stringNumber) > threshold) {
     console.log(`Проверка ${stringNumber} больше, чем пороговое`)
 } else {
     console.log(`Проверка ${stringNumber} меньше или равно ${threshold}. Неудача.`)
 }
+
+// part 2 
 
 console.log('part 2')
 
@@ -31,18 +33,18 @@ let ticketType = "premium"
 
 switch (ticketType) {
     case "premium":
-        console.log( 'Доступны все зоны' );
+        console.log('Доступны все зоны')
         break;
     case "standard":
-    case "economy":   
-        console.log( 'Доступна только основная зона' );
+    case "economy":
+        console.log('Доступна только основная зона')
         break;
     default:
         console.log('Нет таких значений')
 }
 
 let dayNumber = 1
-switch (dayNumber){
+switch (dayNumber) {
     case 1:
         console.log('Понедельник')
         break
@@ -73,3 +75,37 @@ let isLightOn = true;
 let statusMessage = isLightOn ? "Свет включен" : "Свет выключен";
 
 console.log(statusMessage);
+
+// part 3 
+console.log('part 3')
+
+let age = 35
+let isStudent = false
+let hasLoyaltyCard = true
+
+if (age > 65 || isStudent === false || hasLoyaltyCard === true) {
+    console.log('Клиент имеет право на скидку')
+} else {
+    console.log('Клиент не имеет права на скидку')
+}
+
+let userRole = "admin"
+let isPaidSubscription = true
+let userLevel = 8
+
+if (userRole === "admin" && isPaidSubscription === true && userLevel >= 5) {
+    console.log('Полный доступ разрешен')
+} else {
+    console.log('Доступ ограничен')
+}
+
+let isLoggedIn = false
+let isBanned = false
+
+if (!isBanned) {
+    console.log('Учетная запись заблокирована.')
+} else if (!isLoggedIn) {
+    console.log('Пожалуйста, авторизуйтесь.')
+} else {
+    console.log('Добро пожаловать!')
+}
