0a0ee13fb170b264a80aff7e3b4ed9b94cd16983
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2025-11-16 18:27:57 +0600

    part2 finished

--- js/conditions/main.js
+++ js/conditions/main.js
@@ -24,3 +24,52 @@ if (Number(stringNumber) > threshold ){
 } else {
     console.log(`Проверка ${stringNumber} меньше или равно ${threshold}. Неудача.`)
 }
+
+console.log('part 2')
+
+let ticketType = "premium"
+
+switch (ticketType) {
+    case "premium":
+        console.log( 'Доступны все зоны' );
+        break;
+    case "standard":
+    case "economy":   
+        console.log( 'Доступна только основная зона' );
+        break;
+    default:
+        console.log('Нет таких значений')
+}
+
+let dayNumber = 1
+switch (dayNumber){
+    case 1:
+        console.log('Понедельник')
+        break
+    case 2:
+        console.log('Вторник')
+        break
+    case 3:
+        console.log('Среда')
+        break
+    case 4:
+        console.log('Четверг')
+        break
+    case 5:
+        console.log('Пятница')
+        break
+    case 6:
+        console.log('Суббота')
+        break
+    case 7:
+        console.log('Воскресенье')
+        break
+    default:
+        console.log('Введите корректное значение дня недели')
+}
+
+let isLightOn = true;
+
+let statusMessage = isLightOn ? "Свет включен" : "Свет выключен";
+
+console.log(statusMessage);
