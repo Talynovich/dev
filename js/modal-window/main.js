b802ef5018ac4ce4335935b3eb8f290ae2e3f557
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2025-12-22 16:20:15 +0600

    added class hidden

--- js/modal-window/main.js
+++ js/modal-window/main.js
@@ -2,16 +2,18 @@ const btn = document.querySelector('button')
 const modal = document.querySelector('.modal')
 const closeBtn = document.querySelector('.closeBtn')
 
+
+
 btn.addEventListener('click', () => {
-    modal.style.display = 'flex'
+    modal.classList.add('hidden')
 })
 
 closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none'
+    modal.classList.remove('hidden')
 })
 
 window.addEventListener('click', event => {
     if (event.target == modal){
-        modal.style.display = 'none'
+        modal.classList.remove('hidden')
     }
 })
