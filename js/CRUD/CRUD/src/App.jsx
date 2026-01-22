a1a48ce208723d91ee53f4d008fc90f42e21e171
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2026-01-23 00:04:31 +0600

    added prettier

--- js/CRUD/CRUD/src/App.jsx
+++ js/CRUD/CRUD/src/App.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {useRef, useState} from "react";
+import { useRef, useState } from 'react'
 
 const data = [
   {
@@ -17,7 +17,7 @@ const data = [
     id: 3,
     title: 'Study',
     completed: false,
-  }
+  },
 ]
 
 function App() {
@@ -29,7 +29,10 @@ function App() {
   }
   const addNewTodo = () => {
     if (value !== '') {
-      setTodos([...todos, {id: todos.length + 1, title: value, completed: false}])
+      setTodos([
+        ...todos,
+        { id: todos.length + 1, title: value, completed: false },
+      ])
       setValue('')
     }
   }
@@ -38,38 +41,56 @@ function App() {
     setTodos(newTodos)
   }
 
-
   return (
     <>
       <div className="container p-4">
         <div className="row">
           <div className="col-4 offset-4">
             <div className="d-flex mb-3">
-              <input type="text" className="form-control mb-2 mr-sm-2" id="todo" placeholder="add todo mame"
-                     value={value} onChange={handleChange}/>
-              <button type="submit" className="btn btn-primary mb-2 ms-2 flex-shrink-0" onClick={addNewTodo}>Add new
-                todo
+              <input
+                type="text"
+                className="form-control mb-2 mr-sm-2"
+                id="todo"
+                placeholder="add todo mame"
+                value={value}
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                className="btn btn-primary mb-2 ms-2 flex-shrink-0"
+                onClick={addNewTodo}
+              >
+                Add new todo
               </button>
             </div>
             <ul className="list-group">
-              {
-                todos.map((todo) => {
-                  return (
-                    <li key={todo.id}
-                        className={classNames(`list-group-item d-flex justify-content-between align-items-center`, {
-                          'bg-success': todo.completed
-                        })}>
-                      <span>{todo.title}</span>
-                      <div>
-                        <button type="button" className="btn btn-success me-2">Done</button>
-                        <button type="button" className="btn btn-danger"
-                                onClick={() => handleDeleteTodo(todo.id)}>Delete
-                        </button>
-                      </div>
-                    </li>
-                  )
-                })
-              }
+              {todos.map((todo) => {
+                return (
+                  <li
+                    key={todo.id}
+                    className={classNames(
+                      `list-group-item d-flex justify-content-between align-items-center`,
+                      {
+                        'bg-success': todo.completed,
+                      }
+                    )}
+                  >
+                    <span>{todo.title}</span>
+                    <div>
+                      <button type="button" className="btn btn-success me-2">
+                        Done
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => handleDeleteTodo(todo.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+                )
+              })}
             </ul>
           </div>
         </div>
