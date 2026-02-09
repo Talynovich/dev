2f665714cfd13866a1d2f79dc5d4be1343b1c0a7
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2026-02-09 09:21:35 +0600

    added react-router headerPage footerPage LayoutPage

---
+++ js/vite-patients/vite-patients/src/pages/LayoutPage/LaoutPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Header from '../header'
+import Footer from '../footer/index.js'
+
+const Laout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Laout
