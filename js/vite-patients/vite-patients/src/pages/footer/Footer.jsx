e31faf9cefedb225916b12396f77ce92312b2f57
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2026-02-10 17:29:56 +0600

    edited footer

--- js/vite-patients/vite-patients/src/pages/footer/Footer.jsx
+++ js/vite-patients/vite-patients/src/pages/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-50 border-t border-gray-200 ">
       <div className="max-w-7xl mx-auto px-4 py-12">
         <p className="text-gray-400 text-xs text-center">
-          © 2026 Медицинский Центр. Лицензия № ЛО-77-01-000000.
+          © 2026 Медицинский Центр.
         </p>
       </div>
     </footer>
