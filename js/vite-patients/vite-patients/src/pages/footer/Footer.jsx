a3d7f30cae12f39d74396f518c7f2cf81e338918
Анатолий Бервенов <a.bervenov@team.avguro.ru>
2026-02-10 18:26:06 +0600

    added current year

--- js/vite-patients/vite-patients/src/pages/footer/Footer.jsx
+++ js/vite-patients/vite-patients/src/pages/footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+  const today = new Date()
+  const year = today.getFullYear()
+
   return (
     <footer className="bg-gray-50 border-t border-gray-200 ">
       <div className="max-w-7xl mx-auto px-4 py-12">
         <p className="text-gray-400 text-xs text-center">
-          © 2026 Медицинский Центр.
+          © {year} Медицинский Центр.
         </p>
       </div>
     </footer>
